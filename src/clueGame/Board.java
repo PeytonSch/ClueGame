92a9877c274450bf696984afcfc1604a7234ec41
James Hawn <jameshawn@mymail.mines.edu>
2019-03-05 11:35:09 -0700

    Board.java skeleton class complete

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -1,9 +1,49 @@
 package clueGame;
 
+import java.util.Map;
+import java.util.Set;
+
+import experiment.BoardCell;
+
 public class Board {
 	
+	private int numRows;
+	private int numColumns;
 	public static final int MAX_BOARD_SIZE = 23;
 	
+	private BoardCell[][] board;
+	private Map<Character, String> legend;
+	private Map<BoardCell, Set<BoardCell>> adjMtx;
+	private Set<BoardCell> targets;
+	private String boardConfigFile;
+	private String roomConfigFile;
 	
+	public Board() {
+		board = new BoardCell[23][23];
+	}
+	
+	public Board getInstance() {
+		return this;
+	}
+	
+	public void initialize() {
+		
+	}
+	
+	public void loadRoomConfig() {
+		
+	}
+	
+	public void loadBoardConfig() {
+		
+	}
+	
+	public void calcAdjacencies() {
+		
+	}
+	
+	public void calcTargets(BoardCell cell, int pathLength) {
+		
+	}
 
 }
