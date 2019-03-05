98c27b575bdc3bbe282da456068c339837cf5a87
James Hawn <jameshawn@mymail.mines.edu>
2019-03-05 11:15:23 -0700

    added C13 files

---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,19 @@
+/*
+ * Authors:
+ * Peyton Scherschel
+ * James Hawn
+ */
+
+package clueGame;
+
+public class BoardCell {
+	
+	private int row;
+	private int col;
+	
+	public BoardCell(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+
+}
