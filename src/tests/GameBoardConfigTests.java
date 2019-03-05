673eccb4820debf411ee5077475cb3076effcf85
Peyton Scherschel <peytonscherschel@mines.edu>
2019-03-05 11:39:50 -0700

    finished @BeforeClass for GameBoardConfigTests and added testBoardDimensions

--- src/tests/GameBoardConfigTests.java
+++ src/tests/GameBoardConfigTests.java
@@ -2,7 +2,10 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import clueGame.Board;
 
 public class GameBoardConfigTests {
 
@@ -10,14 +13,27 @@ public class GameBoardConfigTests {
 	public static final int LEGEND_SIZE = 11;
 	public static final int NUM_ROWS = 22;
 	public static final int NUM_COLUMNS = 23;
-	
+
 	private static Board board;
-	
+
 	@BeforeClass
-	public static void setUp();
-	board = Board.getInstance();
-	
-	
-	
+	public static void setUp() {
+		board = Board.getInstance();
+
+		//using our teams files
+		board.setConfigFiles("ClueGameLayout.csv","ClueRooms.txt");
+		//load both config files
+		board.initialize();
+	}
+
+	@Test
+	public void testBoardDimensions() {
+		// Ensure we have the proper number of rows and columns
+		assertEquals(NUM_ROWS, board.getNumRows());
+		assertEquals(NUM_COLUMNS, board.getNumColumns());		
+	}
+
+
+
 
 }
