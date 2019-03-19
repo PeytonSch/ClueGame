5a02afbeacdf759eb5c4d97fc7240fbf1c4d8163
Peyton Scherschel <peytonscherschel@mines.edu>
2019-03-19 11:42:25 -0600

    Created gameSetupTests.java

---
+++ src/tests/gameSetupTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class gameSetupTests {
+
+	
+	//do once before running any tests
+	@BeforeClass
+	public static void setUp() {
+		
+	}
+	//do before each test
+	@Before
+	public static void reset() {
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
