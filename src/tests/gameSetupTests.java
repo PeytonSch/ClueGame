8089150ba96485015d6aa6ba02ff81c09006631c
Peyton Scherschel <peytonscherschel@mines.edu>
2019-03-21 11:40:34 -0600

    finished writing testPlayersLoaded. Test do not pass yet.

--- src/tests/gameSetupTests.java
+++ src/tests/gameSetupTests.java
@@ -9,6 +9,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import clueGame.Board;
+import clueGame.Player;
 
 public class gameSetupTests {
 private static Board board;
@@ -31,9 +32,9 @@ private static Board board;
 	//test loading the people 
 	@Test
 	public void testPlayersLoaded() {
-		
+		Player humanPlayer = players.get(1);
 		//test that the correct number of players was loaded
-		
+		assertTrue(players.length == 6);
 		/*
 		 * The following test will test different players for correct 
 		 * Name, Color, Human/Computer, Start Location
@@ -47,10 +48,20 @@ private static Board board;
 		assertTrue(humanPlayer.getStartLocation == aLocation)
 		
 		//test the first player in file 
-		
+		assertTrue(players.get(0).getColor == color);
+		assertTrue(players.get(0).getName == name)
+		assertTrue(players.get(0).getTypeOfPlayer == human)
+		assertTrue(players.get(0).getStartLocation == aLocation)
 		//test the third player in file
-		
+		assertTrue(players.get(2).getColor == color);
+		assertTrue(players.get(2).getName == name)
+		assertTrue(players.get(2).getTypeOfPlayer == human)
+		assertTrue(players.get(2).getStartLocation == aLocation)
 		//test the last player in file
+		assertTrue(players.get(players.length).getColor == color);
+		assertTrue(players.get(players.length).getName == name)
+		assertTrue(players.get(players.length).getTypeOfPlayer == human)
+		assertTrue(players.get(players.length).getStartLocation == aLocation)
 	}
 	
 	
